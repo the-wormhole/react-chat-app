613cfc515545f6fb37f5e62188213ff7eea86fcc
the-wormhole <nayan.agg13@gmail.com>
2024-03-29 07:52:34 +0530

    Setting up create chat page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import {useState} from 'react'
 
-function App() {
+function Home(){
   return (
     <div className='container'>
       <span className='size'>Welcome to the Gossip Engine 😈</span>
@@ -17,4 +18,60 @@ function App() {
   );
 }
 
-export default App;
+
+export function CreateChat(){
+
+  // Math.random()*100000
+  const [cname,setCname] = useState("");
+  const [uname,setUname] = useState("");
+
+  function create(event){
+  
+    event.preventDefault();
+    console.log(event);
+    if(!cname || !uname){
+      console.log(cname);
+      console.log(uname)
+      window.alert(`Chat Name and User Name both are required!!`);
+      return;
+    }
+  
+    //make request to websockets to create chat
+    window.alert(`${cname} was create by user - ${uname}`);
+  }
+
+  return(
+    <div>
+      <h1>Create</h1>
+      <form onSubmit={create}>
+
+        <input 
+          id='cname'
+          name='cname'
+          type="text" 
+          value={cname}
+          placeholder='Chat Name'
+          onChange={(e) =>{setCname(e.target.value)}}
+          required 
+          />
+
+        <input 
+          id='uname' 
+          name='uname' 
+          type="text"
+          value={uname}
+          placeholder='User Name'
+          required
+          onChange={(e) =>{setUname(e.target.value)}}
+          />
+        <button type='submit'>Create</button>
+      </form>
+    </div>
+  )
+}
+
+export function App() {
+  //setTimeout(3000);
+  return <Home/>;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App,CreateChat} from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/create' element={<CreateChat />}/>
+      <Route path='/' element={<App />}/>
+    </Routes>
+    {/* <App /> */}
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
